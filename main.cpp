37b9672e61fd60eb5a5e097ede45b5df6efaac62
shehel <shehel@live.com>
2016-12-17 13:40:58 +0000

    Have camera movement, need to do strafing

--- main.cpp
+++ main.cpp
@@ -34,6 +34,8 @@ float tilt = 0;
 float deltaAngle = 0.0f;
 float deltaMove = 0;
 
+int xOrigin=256;
+
 unsigned int g_program_obj = 0;
 unsigned int g_vertex_obj = 0;
 unsigned int g_fragment_obj = 0;
@@ -41,6 +43,16 @@ unsigned int g_fragment_obj = 0;
 int g_ticks = 0; // keeps couting
 int v_ticks_loc = 0; // location of ticker in vertex shader
 int v_toggle_loc = 0; // location of action toggle flag
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, 1.0f, 0.1f, 100.0f);
+
+	glutPostRedisplay();
+}
 
 
 void wall(float x1, float x2, float y1, float y2, float z1, float z2){
@@ -108,93 +120,26 @@ void computePos(float deltaMove) {
 	z += deltaMove * lz * 0.1f;
 }
 
-void computeDir(float deltaAngle) {
+/*void computeDir(float deltaAngle) {
 
 	angle += deltaAngle;
 	lx = sin(angle);
 	lz = -cos(angle);
-}
+}*/
 
 void idle()
 {
-	bool post = false;
-
-    usleep(10000); // in microseconds
-
-	// restrict to range 0 to 999
-	g_ticks = (g_ticks+1)%1000;
-
-	// if shader program is installed
-	if (glIsProgram(g_program_obj))
-	{
-		glUniform1i(v_ticks_loc, g_ticks); // send to vertex shader
-		post = true;
-	}
-
-	if (g_spinning)
-	{
-    	g_angle += 1;
-		post = true;
-	}
-
-	// only redraw if we need to
-	if (post)
-    	glutPostRedisplay();
+ bool post = false;
 
 }
 
-void keyboard(unsigned char key, int, int)
-{
-	float fraction = 0.1;
-	//std::cerr << "\t you pressed the " << key << " key" << std::endl;
-	//std::cerr << "\t x coordinate: " << x << " y coordinate: " << y << std::endl;
 
-	switch (key)
-	{
-		//case 's': eyey = eyey - 0.10f; break;
 
-		case 'a': angle -= 0.1f;
-			lx = sin(angle);
-			lz = -cos(angle);
-			break;
-
-		case 'd': angle += 0.1f;
-		 	lx = sin(angle);
-		 	lz = -cos(angle);
-		 	break;
-
-		// clockwise rotate
-		//case 'w': eyey = eyey + 0.10f; break;
-
-		case 'w': x += lx * fraction;
-			z += lz * fraction;
-			break;
-
-		case 's': x -= lx * fraction;
-			z -= lz * fraction;
-			break;
-
-		case 'q': tilt += 0.1;
-			break;
-		case 'Q': tilt -= 0.1;
-			break;
-
-		case ' ': y = y + 0.5;
-			break;
-
-		case 'T': y = 1;
-			break;
-	}
-
-	glutPostRedisplay(); // force a redraw
-}
-
-void display()
+void display(void)
 {
 	if (deltaMove)
 		computePos(deltaMove);
-	if (deltaAngle)
-		computeDir(deltaAngle);
+
 
 
 	glMatrixMode(GL_MODELVIEW);
@@ -212,27 +157,9 @@ void display()
 	// position and orient camera
 
 
-	/*
-	glColor3f(0.9f, 0.9f, 0.9f);
-	glBegin(GL_QUADS);
-		glVertex3f(-100.0f, 0.0f, -100.0f);
-		glVertex3f(-100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f,  100.0f);
-		glVertex3f( 100.0f, 0.0f, -100.0f);
-	glEnd();
-	*/
-	//glRotatef(g_angle, 0, 1, 0);
 
-	if (glIsProgram(g_program_obj))
-		glUniform1i(v_toggle_loc, true); // turn on vertex rotation
 
-	//wall(0.1, -0.1, 0.1, -0.1, 0.01f, -0.01f);
-//
-	if(glIsProgram(g_program_obj))
-		glUniform1i(v_toggle_loc, false); // turn off vertex rotation
 
-	//glColor3f(0.8f, 0.8f, 0.8f);
-	//glutWireCube(1.0f);
 
 	//TODO Draw the maze here
 	int a[6][6] = {
@@ -253,7 +180,7 @@ void display()
 		icorrect = i + 3;
 		for(int j=-3; j < 3; j++) {
 			jcorrect = j + 3;
-			std::cerr << a[i][j] <<std::endl;
+			//std::cerr << a[icorrect][jcorrect] <<std::endl;
 			if (a[icorrect][jcorrect] == 0) {
 				glPushMatrix();
 				glTranslatef(2*i, 0, j*2);
@@ -270,221 +197,91 @@ void display()
 				drawFloor(1, -1, 1, -1);
 				glPopMatrix();
 			}
-
-			//x++;
-
 		}
-
 	}
-	/*glPushMatrix();
-				glTranslatef(0, 0, 0);
-				wall(1, -1, 1, -1, 1, -1);
-
-			glPopMatrix();
-				glPushMatrix();
-
-glTranslatef(3.5, 0, 0);
-				wall(1, -1, 1, -1, 1, -1);
-				glPopMatrix();
-*/
 	glutSwapBuffers();
 }
 
-void reshape(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0, 1.0f, 0.1f, 100.0f);
-
-	glutPostRedisplay();
-}
 
 
 
-char* read_shader_source(const char* filename)
-{
-	char* buffer = NULL;
+void pressKey(unsigned char key, int xx, int yy) {
+				std::cerr << key <<std::endl;
+	switch (key) {
 
-	FILE* fp = fopen(filename, "r");
-	if (fp!=NULL)
-	{
-		struct stat status_buf;
-		stat(filename, &status_buf); // find out how big it is
+		case 'w' : deltaMove = 0.5f; break;
+		case 's' : deltaMove = -0.5f; break;
 
-		buffer = new char[status_buf.st_size+1];
-		// read in the file
-		fread(buffer, 1, status_buf.st_size, fp);
-		buffer[status_buf.st_size] = '\0'; // null terminate it
-
-		fclose(fp);
-	}
-	else
-	{
-		fprintf(stderr, "Failed to open shader file %s for reading\n", filename);
-		exit(1);
-	}
-
-	return buffer;
-}
-
-void print_shader_info_log(unsigned int shader_obj)
-{
-	int len = 0;
-	glGetShaderiv(shader_obj, GL_INFO_LOG_LENGTH, &len);
-	if (len>1)
-	{
-		char* log = new char[len];
-		int s_len = 0;
-		glGetShaderInfoLog(shader_obj, len, &s_len, log);
-		fprintf(stderr, "%s", log);
-		delete[] log;
 	}
 }
 
-void print_program_info_log(unsigned int shader_obj)
-{
-	int len = 0;
-	glGetProgramiv(shader_obj, GL_INFO_LOG_LENGTH, &len);
-	if (len>1)
-	{
-		char* log = new char[len];
-		int s_len = 0;
-		glGetProgramInfoLog(shader_obj, len, &s_len, log);
-		fprintf(stderr, "%s", log);
-		delete[] log;
+void releaseKey(unsigned char key, int x, int y) {
+
+	switch (key) {
+
+		case 'w' :
+		case 's' : deltaMove = 0;break;
 	}
 }
 
-void create_and_compile_shaders(
-		const char* vertex_shader_filename,
-		const char* fragment_shader_filename
-	)
-{
-	fprintf(stderr, "Shading Language version %s\n",
-			glGetString(GL_SHADING_LANGUAGE_VERSION));
+void mouseButton(int button, int state, int x, int y) {
 
-	// read the shader source files
-	char* vertex_source = read_shader_source(vertex_shader_filename);
-	char* frag_source = read_shader_source(fragment_shader_filename);
+	// only start motion if the left button is pressed
+	if (button == GLUT_LEFT_BUTTON) {
 
-	if (vertex_source && frag_source)
-	{
-		// create shader program object and shader objects
-		g_vertex_obj = glCreateShader(GL_VERTEX_SHADER);
-		g_fragment_obj = glCreateShader(GL_FRAGMENT_SHADER);
-
-
-		// put sources into shader objects
-		glShaderSource(g_vertex_obj, 1, (const char**)&vertex_source, NULL);
-		glShaderSource(g_fragment_obj, 1, (const char**)&frag_source, NULL);
-
-		// attempt to compile and link
-		glCompileShader(g_vertex_obj);
-
-		// check if it has compile ok
-		int compiled = 0;
-		glGetShaderiv(g_vertex_obj, GL_COMPILE_STATUS, &compiled);
-		if (compiled==0)
-		{
-			// failed to compile vertex shader
-			fprintf(stderr, "Failed to compile vertex shader\n");
-			print_shader_info_log(g_vertex_obj);
-
-			exit(1);
+		// when the button is released
+		if (state == GLUT_UP) {
+			angle -= deltaAngle;
+			xOrigin = -1;
 		}
-
-		glCompileShader(g_fragment_obj);
-		glGetShaderiv(g_fragment_obj, GL_COMPILE_STATUS, &compiled);
-		if (compiled==0)
-		{
-			// failed to compile fragment shader
-			fprintf(stderr, "Failed to compile fragment shader\n");
-			print_shader_info_log(g_fragment_obj);
-			exit(1);
+		else {// state = GLUT_DOWN
+			xOrigin = x;
 		}
-
-		// attach shaders to the program object
-		g_program_obj = glCreateProgram();
-		glAttachShader(g_program_obj, g_vertex_obj);
-		glAttachShader(g_program_obj, g_fragment_obj);
-
-		// try to link the program
-		glLinkProgram(g_program_obj);
-
-		int linked = 0;
-		glGetProgramiv(g_program_obj, GL_LINK_STATUS, &linked);
-		if (linked==0)
-		{
-			// failed to link program
-			fprintf(stderr, "Failed to link shader program\n");
-			print_program_info_log(g_program_obj);
-			exit(1);
-		}
-
-		delete[] vertex_source;
-		delete[] frag_source;
 	}
-
-	if (glIsProgram(g_program_obj))
-		glUseProgram(g_program_obj);
 }
 
-void init(int argc, char* argv[])
-{
-	if (argc>2)
-	{
-		create_and_compile_shaders(argv[1], argv[2]);
+void mouseMove(int x, int y) {
 
-		// get the location of the ticks variable
-		v_ticks_loc = glGetUniformLocation(g_program_obj, "v_ticks");
 
-		// get the location of an action toggle
-		v_toggle_loc = glGetUniformLocation(g_program_obj, "v_toggle");
 
+	if (xOrigin >= 0) {
+		std::cerr << "ALl Eyes" << xOrigin <<std::endl;
+		deltaAngle = (x - xOrigin) * 0.005f;
+
+		lx = sin(angle + deltaAngle);
+		lz = -cos(angle + deltaAngle);
 	}
-
-	//glMatrixMode (GL_PROJECTION);
-	//glEnable(GL_DEPTH_TEST);
-	//glLoadIdentity ();
-
-	//glOrtho(-1.0, 1.0, -1.0, 1.0, -3.0, 1.0);
-
-	glEnable(GL_DEPTH_TEST);
 }
+int main(int argc, char **argv) {
 
-int main(int argc, char* argv[])
-{
+	// init GLUT and create window
 	glutInit(&argc, argv);
-
-	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
-
-	glutInitWindowSize(512, 512);
-	glutInitWindowPosition(50, 50);
-
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(512,512);
 	glutCreateWindow("3D Maze");
 
-#ifndef __APPLE__
-	GLenum err = glewInit();
-	if (GLEW_OK!=err)
-	{
-		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-		exit(1);
-	}
-
-	fprintf(stderr, "Using GLEW %s\n", glewGetString(GLEW_VERSION));
-#endif
-
-	glutKeyboardFunc(keyboard);
-	glutReshapeFunc(reshape);
+	//Callbacks
 	glutDisplayFunc(display);
-	glutIdleFunc(idle);
-
-	fprintf(stderr, "Open GL version %s\n", glGetString(GL_VERSION));
-	init(argc, argv);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(display);
 
 
+	//Keyboard callbacks
+	glutKeyboardFunc(pressKey);
+	glutIgnoreKeyRepeat(1);
+	glutKeyboardUpFunc(releaseKey);
+
+	//Mouse Callbacks
+	glutMouseFunc(mouseButton);
+	glutPassiveMotionFunc(mouseMove);
+	 glutSetCursor(GLUT_CURSOR_NONE);
+
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
+
+	// enter GLUT event processing cycle
 	glutMainLoop();
 
-	return 0;
+	return 1;
 }
